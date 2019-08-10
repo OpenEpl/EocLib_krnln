cab538e6603ec2d456c8f7d1049ab2859749cb42
qiqiworld <1354092549@qq.com>
2019-08-10 22:14:02 +0800

    添加命令 加入成员/删除成员

--- krnln/src/e/lib/krnln/ArrayUtils.h
+++ krnln/src/e/lib/krnln/ArrayUtils.h
@@ -3,33 +3,64 @@
 #include <e/system/func.h>
 namespace e
 {
-	namespace lib
-	{
-		namespace krnln
-		{
-			namespace ArrayUtils
-			{
-				template <typename TElem, typename... TDims> void Redim(e::system::array<TElem> &arr, std::optional<bool> preserve, TDims... dims)
-				{
-					arr.Redim(preserve.value_or(false), size_t(dims)...);
-				}
-				template <typename TElem> intptr_t GetSize(const e::system::array<TElem> &arr)
-				{
-					return intptr_t(arr.GetSize());
-				}
-				template <typename TElem> intptr_t GetUBound(const e::system::array<TElem> &arr, std::optional<intptr_t> dimension = 1)
-				{
-					return arr.GetUBound(size_t(dimension.value_or(1)));
-				}
-				template <typename TElem> void CopyTo(e::system::array<TElem> &dest, const e::system::array<TElem> &src)
-				{
-					dest = src;
-				}
-				template <typename TElem> void Clear(e::system::array<TElem> &arr)
-				{
-					arr = nullptr;
-				}
-			}
-		}
-	}
+    namespace lib
+    {
+        namespace krnln
+        {
+            namespace ArrayUtils
+            {
+                template <typename TElem, typename... TDims>
+                void Redim(e::system::array<TElem> &arr, std::optional<bool> preserve, TDims... dims)
+                {
+                    arr.Redim(preserve.value_or(false), size_t(dims)...);
+                }
+                template <typename TElem>
+                intptr_t GetSize(const e::system::array<TElem> &arr)
+                {
+                    return intptr_t(arr.GetSize());
+                }
+                template <typename TElem>
+                intptr_t GetUBound(const e::system::array<TElem> &arr, std::optional<intptr_t> dimension = 1)
+                {
+                    return arr.GetUBound(size_t(dimension.value_or(1)));
+                }
+                template <typename TElem>
+                void CopyTo(e::system::array<TElem> &dest, const e::system::array<TElem> &src)
+                {
+                    dest = src;
+                }
+                template <typename TElem>
+                void Clear(e::system::array<TElem> &arr)
+                {
+                    arr = nullptr;
+                }
+                template <typename TElem, typename... TArgs>
+                void Append(e::system::array<TElem> &arr, TArgs... args)
+                {
+                    arr.Append(std::forward<TArgs>(args)...);
+                }
+                template <typename TElem>
+                void Remove(e::system::array<TElem> &arr, intptr_t start, std::optional<intptr_t> count = std::nullopt)
+                {
+                    auto count_v = count.value_or(1);
+                    if (start <= 0 || count_v <= 0)
+                        return;
+                    auto size = arr.GetSize();
+                    if (size_t(start) > size)
+                        return;
+                    if (size_t(start) + size_t(count_v) - 1 > size)
+                        count_v = intptr_t(size) - start + 1;
+                    auto numOfMoving = size - size_t(start) - size_t(count_v) + 1;
+                    TElem *startMoving = &arr.GetElemPtr()[size_t(start) + size_t(count_v) - 1];
+                    TElem *target = &arr.GetElemPtr()[start - 1];
+                    for (size_t i = 0; i < numOfMoving; i++)
+                    {
+                        target[i] = std::move(startMoving[i]);
+                    }
+                    size -= count_v;
+                    arr.Redim(true, size);
+                }
+            }
+        }
+    }
 }
--- krnln/test/TestForArrayUtils.cpp
+++ krnln/test/TestForArrayUtils.cpp
@@ -3,19 +3,32 @@
 using namespace e::lib::krnln;
 TEST_CASE("ArrayUtils", "[ArrayUtils]")
 {
-	e::system::array<int> test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	REQUIRE(ArrayUtils::GetSize(test) == 10);
-	REQUIRE(ArrayUtils::GetUBound(test) == 10);
-	REQUIRE(ArrayUtils::GetUBound(test, 1) == 10);
-	ArrayUtils::Redim(test, true, 2);
-	REQUIRE(ArrayUtils::GetSize(test) == 2);
-	REQUIRE(test.At_CStyle(1) == 2);
-	{
-		e::system::array<int> bar;
-		ArrayUtils::CopyTo(bar, test);
-		REQUIRE(ArrayUtils::GetSize(bar) == 2);
-		REQUIRE(bar.At_CStyle(1) == 2);
-	}
-	ArrayUtils::Clear(test);
-	REQUIRE(ArrayUtils::GetSize(test) == 0);
+    e::system::array<int> test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    REQUIRE(ArrayUtils::GetSize(test) == 10);
+    REQUIRE(ArrayUtils::GetUBound(test) == 10);
+    REQUIRE(ArrayUtils::GetUBound(test, 1) == 10);
+    ArrayUtils::Redim(test, true, 2);
+    REQUIRE(ArrayUtils::GetSize(test) == 2);
+    REQUIRE(test.At_CStyle(1) == 2);
+    {
+        e::system::array<int> bar;
+        ArrayUtils::CopyTo(bar, test);
+        REQUIRE(ArrayUtils::GetSize(bar) == 2);
+        REQUIRE(bar.At_CStyle(1) == 2);
+    }
+    ArrayUtils::Clear(test);
+    REQUIRE(ArrayUtils::GetSize(test) == 0);
+    ArrayUtils::Append(test, 1, 2, 3);
+    CHECK(test == e::system::array<int>{1, 2, 3});
+}
+
+TEST_CASE("ArrayUtils::Remove", "[ArrayUtils]")
+{
+    e::system::array<int> x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ArrayUtils::Remove(x, 0, 1);
+    CHECK(x == e::system::array<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    ArrayUtils::Remove(x, 1, 2);
+    CHECK(x == e::system::array<int>{3, 4, 5, 6, 7, 8, 9, 10});
+    ArrayUtils::Remove(x, 5, 10000);
+    CHECK(x == e::system::array<int>{3, 4, 5, 6});
 }

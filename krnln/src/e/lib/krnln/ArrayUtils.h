c676995d4f5cf880a698b7860ef974951826c39a
qiqiworld <1354092549@qq.com>
2019-08-12 15:17:51 +0800

    完善 数组操作(insert/sort/init)

--- krnln/src/e/lib/krnln/ArrayUtils.h
+++ krnln/src/e/lib/krnln/ArrayUtils.h
@@ -34,10 +34,35 @@ namespace e
                 {
                     arr = nullptr;
                 }
+                template <typename TElem>
+                void Init(e::system::array<TElem> &arr)
+                {
+                    for (auto &item : arr)
+                    {
+                        item = TElem();
+                    }
+                }
                 template <typename TElem, typename... TArgs>
                 void Append(e::system::array<TElem> &arr, TArgs... args)
                 {
                     arr.Append(std::forward<TArgs>(args)...);
+                }
+                template <typename TElem, typename TActualElem>
+                void Insert(e::system::array<TElem> &arr, intptr_t pos, TActualElem newElem)
+                {
+                    if (pos <= 0)
+                        return;
+                    auto size = arr.GetSize();
+                    if (size_t(pos) > size + 1)
+                        return;
+                    arr.Append(std::move(static_cast<TElem>(newElem)));
+                    auto elem = arr.GetElemPtr();
+                    TElem last = std::move(elem[size]);
+                    for (size_t i = size; i >= size_t(pos); i--)
+                    {
+                        elem[i] = std::move(elem[i - 1]);
+                    }
+                    elem[pos - 1] = std::move(last);
                 }
                 template <typename TElem>
                 void Remove(e::system::array<TElem> &arr, intptr_t start, std::optional<intptr_t> count = std::nullopt)
@@ -59,6 +84,14 @@ namespace e
                     }
                     size -= count_v;
                     arr.Redim(true, size);
+                }
+                template <typename TElem>
+                void Sort(e::system::array<TElem> &arr, std::optional<bool> asc = std::nullopt)
+                {
+                    if (asc.value_or(true))
+                        std::sort(arr.begin(), arr.end(), std::less<TElem>());
+                    else
+                        std::sort(arr.begin(), arr.end(), std::greater<TElem>());
                 }
             }
         }
--- krnln/test/TestForArrayUtils.cpp
+++ krnln/test/TestForArrayUtils.cpp
@@ -32,3 +32,32 @@ TEST_CASE("ArrayUtils::Remove", "[ArrayUtils]")
     ArrayUtils::Remove(x, 5, 10000);
     CHECK(x == e::system::array<int>{3, 4, 5, 6});
 }
+
+TEST_CASE("ArrayUtils::Init", "[ArrayUtils]")
+{
+    e::system::array<int> x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ArrayUtils::Init(x);
+    CHECK(x == e::system::array<int>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+}
+
+TEST_CASE("ArrayUtils::Insert", "[ArrayUtils]")
+{
+    e::system::array<int> x = {1, 2, 3, 4, 5, 6};
+    ArrayUtils::Insert(x, 6, -1.0f);
+    CHECK(x == e::system::array<int>{1, 2, 3, 4, 5, -1, 6});
+    ArrayUtils::Insert(x, 8, -2);
+    CHECK(x == e::system::array<int>{1, 2, 3, 4, 5, -1, 6, -2});
+    ArrayUtils::Insert(x, 0, -3);
+    CHECK(x == e::system::array<int>{1, 2, 3, 4, 5, -1, 6, -2});
+    ArrayUtils::Insert(x, 1, -3);
+    CHECK(x == e::system::array<int>{-3, 1, 2, 3, 4, 5, -1, 6, -2});
+}
+
+TEST_CASE("ArrayUtils::Sort", "[ArrayUtils]")
+{
+    e::system::array<int> x = {3, 0, 2, 7, 5, 320, 8415, 282};
+    ArrayUtils::Sort(x);
+    CHECK(x == e::system::array<int>{0, 2, 3, 5, 7, 282, 320, 8415});
+    ArrayUtils::Sort(x, false);
+    CHECK(x == e::system::array<int>{8415, 320, 282, 7, 5, 3, 2, 0});
+}

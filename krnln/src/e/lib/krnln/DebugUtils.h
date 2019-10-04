0926674cca57b02e785853a179b4f9a4d92aa27a
qiqiworld <1354092549@qq.com>
2019-10-04 14:20:00 +0800

    新增 暂停/检查 命令的支持

--- krnln/src/e/lib/krnln/DebugUtils.h
+++ krnln/src/e/lib/krnln/DebugUtils.h
@@ -43,8 +43,46 @@ namespace e
         }
     }
 }
+
+#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#define EOC_DEBUGBREAK_ALWAYS() __debugbreak()
+#elif defined(__i386__) || defined(__x86_64__)
+static inline void EOC_DEBUGBREAK_ALWAYS()
+{
+    __asm__ __volatile__("int $03");
+}
+#elif defined(__thumb__)
+static inline void EOC_DEBUGBREAK_ALWAYS()
+{
+    __asm__ __volatile__(".inst 0xde01");
+}
+#elif defined(__aarch64__)
+static inline void EOC_DEBUGBREAK_ALWAYS()
+{
+    __asm__ __volatile__(".inst 0xd4200000");
+}
+#elif defined(__arm__)
+static inline void EOC_DEBUGBREAK_ALWAYS()
+{
+    __asm__ __volatile__(".inst 0xe7f001f0");
+}
+#else
+#include <signal.h>
+#if defined(SIGTRAP)
+#define EOC_DEBUGBREAK_ALWAYS() raise(SIGTRAP)
+#else
+#define EOC_DEBUGBREAK_ALWAYS() raise(SIGABRT)
+#endif
+#endif
+
+#define EOC_ASSERT_ALWAYS(condition) if(condition) EOC_DEBUGBREAK_ALWAYS()
+
 #if _DEBUG
 #define EOC_OUTPUT_DEBUG_INFO(...) e::lib::krnln::DebugUtils::OutputDebugInfo(__VA_ARGS__)
+#define EOC_DEBUGBREAK(...) EOC_DEBUGBREAK_ALWAYS(__VA_ARGS__)
+#define EOC_ASSERT(...) EOC_ASSERT_ALWAYS(__VA_ARGS__)
 #else
 #define EOC_OUTPUT_DEBUG_INFO(...)
+#define EOC_DEBUGBREAK(...)
+#define EOC_ASSERT(...)
 #endif

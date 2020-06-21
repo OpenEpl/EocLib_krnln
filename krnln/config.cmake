e36950a22c3241ccdc550d099ac2d3b94497428b
qiqiworld <1354092549@qq.com>
2020-06-21 12:01:26 +0800

    improve: use new eoc lib system

--- krnln/config.cmake
+++ krnln/config.cmake
@@ -5,9 +5,7 @@ if(NOT EocKrnln_FOUND)
 	include(${EOC_HOME}/EocBuildHelper.cmake)
 	add_subdirectory(${CMAKE_CURRENT_LIST_DIR} ${CMAKE_BINARY_DIR}/EocKrnln)
 	
-	include(${EOC_LIBS_DIRS}/system/config.cmake)
-
 	set(EocKrnln_FOUND True)
-	set(EocKrnln_INCLUDE_DIRS ${EocSystem_INCLUDE_DIRS} ${CMAKE_CURRENT_LIST_DIR}/src)
-	set(EocKrnln_LIBRARIES ${EocSystem_LIBRARIES} EocKrnln)
+	set(EocKrnln_INCLUDE_DIRS)
+	set(EocKrnln_LIBRARIES EocKrnln)
 endif()

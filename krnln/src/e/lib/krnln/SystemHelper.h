48f2ae893ca498fb81f0c9299c995eaefe7589dd
qiqiworld <1354092549@qq.com>
2020-07-20 20:24:18 +0800

    fix: 明确ShowMessageBox的重载选择

--- krnln/src/e/lib/krnln/SystemHelper.h
+++ krnln/src/e/lib/krnln/SystemHelper.h
@@ -13,15 +13,15 @@ namespace e
                     int32_t flags,
                     std::optional<std::reference_wrapper<const e::system::string>> title = std::nullopt,
                     std::optional<intptr_t> handle = std::nullopt);
-                template <typename T>
+                template <typename T, std::enable_if_t<!std::is_convertible_v<T, e::system::string>, int> = 0>
                 auto ShowMessageBox(T content,
                     int32_t flags,
                     std::optional<std::reference_wrapper<const e::system::string>> title = std::nullopt,
                     std::optional<intptr_t> handle = std::nullopt)
                 {
-                    return ShowMessageBox(krnln::ToString(std::forward<T>(content)), flags, std::optional(std::cref(title)), handle);
+                    return ShowMessageBox(krnln::ToString(std::forward<T>(content)), flags, title, handle);
                 }
-                template <typename T>
+                template <typename T, std::enable_if_t<!std::is_convertible_v<T, e::system::string>, int> = 0>
                 auto ShowMessageBox(T content,
                     int32_t flags,
                     const e::system::string &title,

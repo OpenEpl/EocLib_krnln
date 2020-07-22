43d0b9227cb35777a4e3980f23e764c9f14f2f1a
qiqiworld <1354092549@qq.com>
2020-07-23 07:30:26 +0800

    feature: support WriteMemory method

--- krnln/src/e/lib/krnln/MemoryUtils.h
+++ krnln/src/e/lib/krnln/MemoryUtils.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <e/system/func.h>
+#include <algorithm>
 namespace e
 {
     namespace lib
@@ -29,6 +30,103 @@ namespace e
                     if (size <= 0)
                         return nullptr;
                     return e::system::make_bin((void *)data, (size_t)size);
+                }
+                inline void CopyMemoryWithLimit(const void *buffer, intptr_t length, intptr_t dest)
+                {
+                    memmove((void *)dest, buffer, (size_t)length);
+                }
+                inline void CopyMemoryWithLimit(const void *buffer, intptr_t length, intptr_t dest, std::optional<intptr_t> limit)
+                {
+                    if (limit.value_or(-1) == -1)
+                    {
+                        CopyMemoryWithLimit(buffer, length, dest);
+                    }
+                    else
+                    {
+                        CopyMemoryWithLimit(buffer, std::min(length, limit.value()), dest);
+                    }
+                }
+                template <typename TSrc, typename... TOthers,
+                    std::enable_if_t<std::is_arithmetic_v<TSrc> || std::is_same_v<TSrc, e::system::methodptr> || std::is_same_v<TSrc, e::system::datetime>, int> = 0>
+                void WriteMemory(TSrc data, TOthers &&... others)
+                {
+                    CopyMemoryWithLimit(&data, sizeof(TSrc), std::forward<TOthers>(others)...);
+                }
+                template <typename... TOthers>
+                void WriteMemory(const e::system::string &data, TOthers &&... others)
+                {
+                    auto length = data.len();
+                    if (length == 0)
+                    {
+                        CopyMemoryWithLimit("", 1, std::forward<TOthers>(others)...);
+                    }
+                    else
+                    {
+                        CopyMemoryWithLimit(data.c_str(), length + 1, std::forward<TOthers>(others)...);
+                    }
+                }
+                template <typename... TOthers>
+                void WriteMemory(const e::system::bin &data, TOthers &&... others)
+                {
+                    CopyMemoryWithLimit(data.GetElemPtr(), data.GetSize(), std::forward<TOthers>(others)...);
+                }
+                template <typename... TOthers>
+                void WriteMemory(const e::system::array<uint8_t> &data, TOthers &&... others)
+                {
+                    CopyMemoryWithLimit(data.GetElemPtr(), data.GetSize(), std::forward<TOthers>(others)...);
+                }
+                template <typename... TOthers>
+                void WriteMemory(const e::system::any &data, TOthers &&... others)
+                {
+                    if (data.type() == typeid(uint8_t))
+                    {
+                        return WriteMemory(data.cast<uint8_t>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(int16_t))
+                    {
+                        return WriteMemory(data.cast<int16_t>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(int32_t))
+                    {
+                        return WriteMemory(data.cast<int32_t>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(int64_t))
+                    {
+                        return WriteMemory(data.cast<int64_t>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(double))
+                    {
+                        return WriteMemory(data.cast<double>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(float))
+                    {
+                        return WriteMemory(data.cast<float>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(bool))
+                    {
+                        return WriteMemory(data.cast<bool>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(e::system::string))
+                    {
+                        return WriteMemory(data.cast<e::system::string>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(e::system::bin))
+                    {
+                        return WriteMemory(data.cast<e::system::bin>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(e::system::methodptr))
+                    {
+                        return WriteMemory(data.cast<e::system::methodptr>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(e::system::datetime))
+                    {
+                        return WriteMemory(data.cast<e::system::datetime>(), std::forward<TOthers>(others)...);
+                    }
+                    if (data.type() == typeid(e::system::array<uint8_t>))
+                    {
+                        return WriteMemory(data.cast<e::system::array<uint8_t>>(), std::forward<TOthers>(others)...);
+                    }
+                    throw std::domain_error("Don't know how to write this object to memory block");
                 }
             }
         }
--- krnln/test/TestForMemoryUtils.cpp
+++ krnln/test/TestForMemoryUtils.cpp
@@ -26,3 +26,16 @@ TEST_CASE("MemoryUtils", "[MemoryUtils]")
         CHECK(MemoryUtils::ReadToBin(dataPtr, 9) == e::system::bin{1, 2, 3, 4, 5, 6, 7, 8, 0});
     }
 }
+
+TEST_CASE("MemoryUtils::WriteMemory", "[MemoryUtils]")
+{
+    std::unique_ptr<uint8_t[]> data = std::make_unique<uint8_t[]>(9);
+    MemoryUtils::WriteMemory(0x1020304050607080LL, (intptr_t)data.get());
+    CHECK((*(int64_t *)data.get()) == 0x1020304050607080LL);
+    MemoryUtils::WriteMemory(EOC_STR_CONST("asd"), (intptr_t)data.get());
+    CHECK(memcmp(data.get(), "asd", 4) == 0);
+    MemoryUtils::WriteMemory(e::system::any(e::system::bin{1, 2, 3}), (intptr_t)data.get());
+    CHECK(memcmp(data.get(), "\x01\x02\x03", 3) == 0);
+    MemoryUtils::WriteMemory(e::system::any(e::system::bin{4, 5, 6}), (intptr_t)data.get(), 2);
+    CHECK(memcmp(data.get(), "\x04\x05\x03", 3) == 0);
+}
